4eaa83001ba9637b7f9efede0fa026bc04b6bf84
Th3Fr3ak <40581656+LOLatom@users.noreply.github.com>
2020-04-16 15:28:04 +0200

    Made : Ash block, Ash Stone, Hot Ash block, Compacted Ash Block, ASH moddedmaterials

--- src/main/java/com/thefreak/botsmod/init/BlockInitNew.java
+++ src/main/java/com/thefreak/botsmod/init/BlockInitNew.java
@@ -5,12 +5,17 @@ import com.thefreak.botsmod.objects.blocks.ChainedDeepWoodBricksBlock;
 import com.thefreak.botsmod.objects.blocks.DeepWoodChestBlock;
 import com.thefreak.botsmod.objects.blocks.MudBlock;
 
+import com.thefreak.botsmod.util.ModdedMaterials;
 import net.minecraft.block.Block;
+import net.minecraft.block.FallingBlock;
+import net.minecraft.block.MagmaBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+
+import java.rmi.registry.Registry;
 
 public class BlockInitNew {
 
@@ -22,4 +27,11 @@ public class BlockInitNew {
 
 	public static final RegistryObject<Block> CHAINED_DEEP_WOOD_BRICKS = BLOCKS.register("chained_deep_wood_bricks", () -> new ChainedDeepWoodBricksBlock(Block.Properties.create(Material.WOOD).sound(SoundType.WOOD).hardnessAndResistance(4f, 12.0f)));
 
+	public static final RegistryObject<Block> ASH_BLOCK = BLOCKS.register("ash_block", () -> new FallingBlock(Block.Properties.create(ModdedMaterials.ASH).sound(SoundType.SAND).hardnessAndResistance(2F, 2F)));
+
+	public static final RegistryObject<Block> ASH_STONE = BLOCKS.register("ash_stone", () -> new Block(Block.Properties.create(ModdedMaterials.ASH).sound(SoundType.SAND).hardnessAndResistance(3F, 2F)));
+
+	public static final RegistryObject<Block> COMPACTED_ASH_BLOCK = BLOCKS.register("compacted_ash_block", () -> new Block(Block.Properties.create(ModdedMaterials.ASH).sound(SoundType.SAND).hardnessAndResistance(3F, 5F)));
+
+	public static final RegistryObject<Block> HOT_ASH_BLOCK = BLOCKS.register("hot_ash_block", () -> new MagmaBlock(Block.Properties.create(ModdedMaterials.ASH).sound(SoundType.SAND).hardnessAndResistance(2F, 15F)));
 }
---
+++ src/main/java/com/thefreak/botsmod/util/ModdedMaterials.java
@@ -0,0 +1,14 @@
+package com.thefreak.botsmod.util;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.block.material.PushReaction;
+
+public class ModdedMaterials {
+    public static final Material DELTACRYSTAL = (new Material.Builder(MaterialColor.EMERALD).doesNotBlockMovement()).build();
+
+    //Falling Materials//
+
+    public static final Material ASH = (new Material.Builder(MaterialColor.BLACK).build());
+
+}
