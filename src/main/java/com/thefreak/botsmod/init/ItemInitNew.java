bef0070255bd65ca64fbb018226e57e5c1f85b18
Th3Fr3ak <40581656+LOLatom@users.noreply.github.com>
2020-04-19 20:11:25 +0200

    Update3 -Sulfur_powder -Cooked_pilove_fruit -Pilove_fruit

--- src/main/java/com/thefreak/botsmod/init/ItemInitNew.java
+++ src/main/java/com/thefreak/botsmod/init/ItemInitNew.java
@@ -1,6 +1,7 @@
 package com.thefreak.botsmod.init;
 
 import com.thefreak.botsmod.BotsMod;
+import com.thefreak.botsmod.objects.items.PiloveFruitItem;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
@@ -20,7 +21,7 @@ public class ItemInitNew {
     public static final RegistryObject<Item> RAW_POISON_BULL_MEAT = ITEMS.register("raw_poison_bull_meat", () -> new Item(new Item.Properties()
             .group(BotsMod.BotsItemGroup.instance)
             .food(new Food.Builder().hunger(2).saturation(0.5F)
-                    .effect(new EffectInstance(Effects.POISON, 400, 2 ), 1F).build())));
+                    .effect(() -> new EffectInstance(Effects.POISON, 200, 2 ), 1).build())));
 
 
     public static final RegistryObject<Item> COOKED_POISON_BULL_MEAT = ITEMS.register("cooked_poison_bull_meat", () -> new Item(new Item.Properties().group(BotsMod.BotsItemGroup.instance).food(new Food.Builder().hunger(5).saturation(1.5F).build())));
@@ -29,5 +30,5 @@ public class ItemInitNew {
 
     public static final RegistryObject<Item> COOKED_PILOVE_FRUIT = ITEMS.register("cooked_pilove_fruit", () -> new Item(new Item.Properties().group(BotsMod.BotsItemGroup.instance).food(new Food.Builder().hunger(2).saturation(2F).build())));
 
-    public static final RegistryObject<Item> PILOVE_FRUIT = ITEMS.register("pilove_fruit", () -> new Item(new Item.Properties().group(BotsMod.BotsItemGroup.instance).food(new Food.Builder().hunger(5).saturation(3F).build())));
+    public static final RegistryObject<Item> PILOVE_FRUIT = ITEMS.register("pilove_fruit", () -> new PiloveFruitItem(new Item.Properties().group(BotsMod.BotsItemGroup.instance).food(new Food.Builder().hunger(5).saturation(3F).effect( () -> new EffectInstance(EffectInitNew.EXPLOSIVE_EFFECT.get(), 400, 1),1.0F).fastToEat().build())));
 }
---
+++ src/main/java/com/thefreak/botsmod/objects/items/PiloveFruitItem.java
@@ -0,0 +1,13 @@
+package com.thefreak.botsmod.objects.items;
+
+import com.mojang.datafixers.util.Pair;
+import net.minecraft.item.Food;
+import net.minecraft.item.Item;
+import net.minecraft.potion.EffectInstance;
+
+public class PiloveFruitItem extends Item {
+    public PiloveFruitItem(Properties properties) {
+        super(properties);
+    }
+
+}
